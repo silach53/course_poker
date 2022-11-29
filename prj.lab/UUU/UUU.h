76540cec696390aa5d75e604fb53446dc4c4af46
silach53 <48329361+silach53@users.noreply.github.com>
2022-11-29 15:52:34 +0300

    second comit

--- prj.lab/UUU/UUU.h
+++ prj.lab/UUU/UUU.h
@@ -11,6 +11,10 @@ public:
 	int place_from_dealer;
 	int money;
 	bool in_game;
+	bool move_is_approved;
+	void make_a_move(){
+		
+	}
 
 	player() :
 		hand({ cards::invalid(), cards::invalid() }),
@@ -22,21 +26,30 @@ public:
 
 class game_state {
 public:
+
+	//Словари перевода цифр в карты и наоборот
 	std::map<int, std::string> cards_val;
 	std::map<int, std::string> suits;
 	std::map<std::string,int> cards_val_r;
 	std::map<std::string, int> suits_r;
+
+	//
 	std::vector<cards> board;
 	int cur_p;//turn_of_player_x
 	int cur_bet;
 	int bank;
+	bool turn_is_active = true;
+	bool show_down_required = false;
 	std::vector<cards> deсk;
 
 	std::pair<cards, cards> random_from_deck() {
-
+		for(auto suit: suits ){
+			for(auto val: cards_val){
+				val.second;
+			}
+		}
 	}
 
-	
 	game_state() : cards_val({ {2, "2"},{3, "3"},{4, "4"},{5, "5"},{6, "6"},{7, "7"},{8, "8"},{9, "9"},{10, "10"},{11, "J"},{12, "Q"},{13, "K"},{14, "A"} }),
 		suits({ {0,"c"},{1,"d"},{2,"h"},{3,"s"} }),
 		cur_p(-1)
@@ -53,35 +66,49 @@ private:
 
 struct cards {
 	int val, suit;
-	cards(int val,int suit):
-		val(val),suit(suit) {}
+	cards(int val,int suit): val(val),suit(suit) {}
+	
+	//Создает карту заглушку
 	cards invalid() {
 		return cards(-1, '-');
 	}
+
+	//Конструктор из карт загрушел
 	cards():val(invalid().val),suit(invalid().suit) {}
 };
 
+void ask_for_move(std::pair<const int,player>){
+	
+}
+
 class game_cycle : public  game_state {
+public:
+
+
 	void seat_the_players(std::map<int, player>& pl,int n) {
 		for (int i = 0; i < n; ++i)
 			pl[i] = player();
 	}
+
 	void deal_cards(std::map<int, player>& pl) {
 		for (int i = 0; i < pl.size(); ++i)
 			players[i].hand = random_from_deck();
 	}
 	
-
-	void game(int number_of_players) {
-		game_state GAME = game_state();
-
-		seat_the_players(GAME.players, number_of_players);
-		deal_cards(GAME.players);
-		
+	//проверяет, не остался ли всего один игрок в игре
+	bool one_left(){
+		int count = 0;
+		for(auto p :players)
+			count += (p.second.money == 0);
+		return (count == 1);
 	}
-};
 
-class actions : public  game_cycle {
+	//Не финальный вариант перехода хода 
+	void hand_over_turn(){
+		cur_p++;
+		cur_p%=players.size();
+	}
+
 	std::string fold() {
 		players[cur_p].in_game = 0;
 
@@ -91,5 +118,55 @@ class actions : public  game_cycle {
 		if (players[cur_p].money < cur_bet) {
 			return "not enough money";
 		}
+
+		players[cur_p].money-=cur_bet;
+
+		return "success";
 	}
+	std::string raise(int raise_money){
+		if (raise_money > cur_bet && players[cur_p].money < raise_money) {
+			return "not enough money";
+		}
+		players[cur_p].money -= raise_money;
+		cur_p++;
+
+		return "success";
+	}
+
+	void make_show_down(){
+
+	}
+
+	void game(int number_of_players = 6) {
+		seat_the_players(players, number_of_players);
+		deal_cards(players);
+		while(one_left()){
+			
+			while(turn_is_active){
+				for(auto p: players){
+
+					if(p.second.in_game == 0) continue;
+
+					//Тут надо запросить у другого кода, при фиксированно состоянии игры сделать ход
+					ask_for_move(p);
+					while(p.second.move_is_approved){
+						try{
+							fold();
+						}
+						catch(std::string Error){}
+					}
+				}
+			}
+
+			if(show_down_required){
+				make_show_down();
+				show_down_required = false;
+			}
+
+		}
+	}
+};
+
+class actions : public  game_cycle {
+	
 };
--- prj.lab/UUU/UUU_test.cpp
+++ prj.lab/UUU/UUU_test.cpp
@@ -2,5 +2,6 @@
 #include <set>
 
 int main() {
-
+    game_cycle GAME;
+    GAME.game();
 }
